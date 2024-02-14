b02505de8f859acb8a104989e271b0246060951b
jean-rocha-santos <jean.santos@sptech.school>
2024-02-14 09:32:34 -0300

    Inicio projeto e exercicios

---
+++ Exercicios/Exercicios/Program.cs
@@ -0,0 +1,89 @@
+﻿//Console.WriteLine("Chute um numero !");
+//Random numero = new Random();
+//int resultado = numero.Next(10);
+
+//while (true)
+//{
+//    string chute = Console.ReadLine()!;
+//    int tentativa = int.Parse(chute);
+
+//    if (tentativa == resultado)
+//    {
+//        Console.WriteLine("Seu chute foi " + chute);
+//        Console.WriteLine("Parabens vc acertou");
+//        break;
+
+//    }
+//    else
+//    {
+//        Console.WriteLine("Seu chute foi " + chute);
+//        Console.WriteLine("Errou");
+//    }
+//}
+//////// Desafio calculadora 
+
+//List<int> numerosInt = new List<int> { 1, 2, 3, 4, 5 };
+
+//foreach (var numeros in numerosInt)
+//{
+//    if (numeros % 2 == 0)
+//    {
+//        Console.Write(numeros + " ");
+//    }
+//}
+//// DESAFIO DICIONARIO 
+
+
+/*Dictionary<string, List<int>> alunos = new Dictionary<string, List<int>>();
+Alunos.Add("Jean", new List<int> { 7, 10, 9 });
+Alunos.Add("Anna", new List<int> { 7, 7, 7 });
+Alunos.Add("Luan", new List<int> { 6, 5, 8 });
+Alunos.Add("Maria", new List<int> { 8, 7, 5 });
+
+for (int aluno = 0; aluno < alunos.Count; aluno ++)
+{
+    Console.WriteLine($"A média do aluno {alunos[aluno]} ");
+}*/
+
+////string NomeDoAluno = Console.ReadLine()!;
+////Console.WriteLine();    
+///
+Dictionary<string, List<int>> vendasCarros = new Dictionary<string, List<int>> {
+    { "Bugatti Veyron", new List<int> { 10, 15, 12, 8, 5 } },
+    { "Koenigsegg Agera RS", new List<int> { 2, 3, 5, 6, 7 } },
+    { "Lamborghini Aventador", new List<int> { 20, 18, 22, 24, 16 } },
+    { "Pagani Huayra", new List<int> { 4, 5, 6, 5, 4 } },
+    { "Ferrari LaFerrari", new List<int> { 7, 6, 5, 8, 10 } }
+};
+int totalBugatti = 0;
+int totalAgera = 0;
+int totalLamborghini= 0;
+int totalPagani = 0;
+int totalFerrari = 0;
+List<int> vendasBugatti = vendasCarros["Bugatti Veyron"];
+List<int> vendasAgera = vendasCarros["Koenigsegg Agera RS"];
+List<int> vendasLamborghini = vendasCarros["Lamborghini Aventador"];
+List<int> vendasPagani = vendasCarros["Pagani Huayra"];
+List<int> vendasFerrari = vendasCarros["Ferrari LaFerrari"];
+for (var venda = 0;venda <vendasCarros.Count;venda++)
+{
+     
+    totalBugatti += vendasBugatti[venda];
+    totalAgera += vendasAgera[venda];
+    totalLamborghini += vendasLamborghini[venda];
+    totalPagani += vendasPagani[venda];
+    totalFerrari += vendasFerrari[venda];
+}
+
+totalBugatti /= vendasBugatti.Count;
+totalAgera /= vendasAgera.Count;
+totalLamborghini/=vendasLamborghini.Count;
+totalPagani /= vendasPagani.Count;
+totalFerrari /= vendasFerrari.Count;
+Console.WriteLine($"Vendas do Bugatti Veyron no primeiro mês: {totalBugatti}");
+Console.WriteLine($"Vendas do Agera no primeiro mês: {totalAgera}");
+Console.WriteLine($"Vendas do Lamborghini  no primeiro mês: {totalLamborghini}");
+Console.WriteLine($"Vendas do Pagani no primeiro mês: {totalPagani}");
+Console.WriteLine($"Vendas do Ferrari no primeiro mês: {totalFerrari}");
+
+
---
+++ Primeiro Projeto/Primeiro Projeto/Program.cs
@@ -0,0 +1,139 @@
+﻿// Screen Sound
+//string curso = "Aprendendo C#";
+
+//Console.WriteLine(curso);
+
+//string nome = "Jean";
+//string sobrenome = "Rocha Santos";
+
+//Console.WriteLine(nome);
+//Console.WriteLine(sobrenome);
+
+
+
+
+///////////////////////////////
+/////
+string msgBoasVindas = "Boas vindas ao Screen Sound";
+//List<string> listasDasBandas = new List<string> { "Charlie Brown","Banda do Mar",
+//    "Gilsons","Legião Urbana" };
+Dictionary<string, List<int>> bandasRegistradas = new Dictionary<string, List<int>>();
+bandasRegistradas.Add("System of Down", new List<int> { 9,10,9});
+bandasRegistradas.Add("Charlie Brown", new List<int>()); 
+void ExibirLogo()
+{
+    Console.WriteLine(@"
+█▀ █▀▀ █▀█ █▀▀ █▀▀ █▄░█   █▀ █▀█ █░█ █▄░█ █▀▄
+▄█ █▄▄ █▀▄ ██▄ ██▄ █░▀█   ▄█ █▄█ █▄█ █░▀█ █▄▀
+");
+    Console.WriteLine(msgBoasVindas); 
+}
+void ExibirOpcoesMenu()
+{
+    Console.Clear();
+    ExibirLogo();
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para visualizar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite -1 para sair");
+
+    Console.Write("\nDigite a sua opção: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumero = int.Parse(opcaoEscolhida);
+  
+    switch (opcaoEscolhidaNumero)
+        // switch quando houver muitos cenarios ao inves de utilizar vairos if e else if 
+    {
+        case 1: RegistrarBanda();
+            break;
+        case 2: MostrarBandas();
+            break;
+        case 3: AvaliarBanda();
+            break;
+        case 4: Console.WriteLine("Você escolheu a opção "+ opcaoEscolhidaNumero );
+            break;
+        case -1: Console.WriteLine("Até a próxima :) "+ opcaoEscolhidaNumero );
+            break;
+        default: Console.WriteLine("Opção inválida");
+            break;
+        // Case para escolher oq cada cenario fará
+        // default caso nenhum cenario anterior for escolhido
+    }
+}
+void RegistrarBanda()
+{
+    Console.Clear();
+    ExibirTituloOpcoes("Registro das bandas");
+    Console.Write("Digite o nome da banda que deseja registrar: ");
+    string nomeBanda = Console.ReadLine()!;
+    ////listasDasBandas.Add(nomeBanda);
+    bandasRegistradas.Add(nomeBanda,new List<int>());
+    Console.WriteLine($"A banda {nomeBanda} foi registrada com sucesso!");
+    Thread.Sleep(2000);
+    Console.Clear();
+    ExibirOpcoesMenu();
+}
+void MostrarBandas()
+{
+    Console.Clear();
+    ExibirTituloOpcoes("Exibindo bandas registradas");
+    ////for (int posicao = 0;posicao < listasDasBandas.Count; posicao++)
+    //{
+    //    Console.WriteLine($"Banda : {listasDasBandas[posicao]}");
+    //}
+    foreach (string banda in bandasRegistradas.Keys)    
+    {
+        Console.WriteLine($"Banda : {banda}");
+    }
+    Console.WriteLine("\nDigite uma tecla para voltar ao menu principal.");
+    Console.ReadKey();
+    Console.Clear();
+    ExibirOpcoesMenu();
+}
+void ExibirTituloOpcoes(string titulo)
+{
+    int qtdLetras = titulo.Length;
+    string asteristicos = string.Empty.PadLeft(qtdLetras, '*');
+    Console.WriteLine(asteristicos);
+    Console.WriteLine(titulo);
+    Console.WriteLine(asteristicos+"\n");
+}
+
+void AvaliarBanda()
+{
+    Console.Clear();
+    ExibirTituloOpcoes("Avaliar Banda!");
+    Console.Write("Digite a banda que quer avaliar: ");
+    string nomeDaBanda =Console.ReadLine()!;
+    if (bandasRegistradas.ContainsKey(nomeDaBanda))
+    {
+        Console.Write($"Qual nota que a banda {nomeDaBanda} merece: ");
+        int nota = int.Parse(Console.ReadLine()!);
+        bandasRegistradas[nomeDaBanda].Add(nota);
+        Console.WriteLine($"\nA nota {nota} da banda {nomeDaBanda} foi registrada com sucesso");
+        ExibirOpcoesMenu();
+    }
+    else
+    {
+        Console.WriteLine($"\nA banda {nomeDaBanda} não foi encontrada");
+        string msgErro="Digite A para tentar novamente, ou digite qualquer tecla para retornar ao menu.";
+        Console.WriteLine(msgErro);
+        string opcaoEscolhida =Console.ReadLine()!.ToUpper();
+        Thread.Sleep(4000);        
+        Console.WriteLine("Teste1");
+
+        if (opcaoEscolhida == "A")
+        {
+            Console.WriteLine("teste");
+            AvaliarBanda();
+        } else 
+        {
+            Console.WriteLine("Teste2");
+            ExibirOpcoesMenu();
+        } 
+    }
+}
+ExibirOpcoesMenu();
+
+
